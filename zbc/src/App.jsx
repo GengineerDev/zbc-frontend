130eba3f872808cd77e6bd24eab432e91cf5be24
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-04-10 16:59:26 +0800

    Add pitch page

--- zbc/src/App.jsx
+++ zbc/src/App.jsx
@@ -2,6 +2,7 @@
 import { Layout } from 'antd'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import PitchPage from './pages/PitchPage'
 const { Header } = Layout
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
@@ -14,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        {/* to add specific paths for the card later */}
+        <Route path="/pitch" element={<PitchPage />} /> 
       </Routes>      
     </Router>
   )
---
+++ zbc/src/pages/PitchPage.jsx
@@ -0,0 +1,19 @@
+import '../styles/pitchPage.css'
+
+function PitchPage(){
+    return (
+        <div className='page'>
+            <center>
+                <img src="https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg" className='pitchImage' />
+            </center>
+            <h1>Pitch Title</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien eget mi. Tortor at risus viverra adipiscing at in tellus. Id interdum velit laoreet id donec. Sed risus ultricies tristique nulla aliquet enim. Eros in cursus turpis massa tincidunt. Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Elementum nibh tellus molestie nunc non blandit massa enim nec. At augue eget arcu dictum varius duis at consectetur lorem.
+
+            Diam sollicitudin tempor id eu nisl nunc. Nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Tempor orci eu lobortis elementum. Integer enim neque volutpat ac tincidunt vitae semper quis. Ante metus dictum at tempor commodo ullamcorper a. Mattis nunc sed blandit libero volutpat sed cras ornare. Sit amet luctus venenatis lectus magna fringilla urna. Amet massa vitae tortor condimentum lacinia quis vel eros donec. Ultricies lacus sed turpis tincidunt. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Et leo duis ut diam quam nulla porttitor massa. Urna nunc id cursus metus aliquam. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Risus at ultrices mi tempus imperdiet nulla malesuada. In nulla posuere sollicitudin aliquam ultrices. Urna nunc id cursus metus aliquam eleifend mi in nulla. Ac turpis egestas sed tempus urna et. Sed libero enim sed faucibus turpis in eu mi. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum.
+            </p>
+        </div>
+    )
+}
+
+export default PitchPage
