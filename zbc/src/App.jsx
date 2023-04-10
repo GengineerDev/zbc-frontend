233d369509f098cfcec6cfb001ba202986195e9d
Gengineer <pedegloriodavidgenesis@gmail.com>
2023-04-10 16:42:11 +0800

    Add Login

--- zbc/src/App.jsx
+++ zbc/src/App.jsx
@@ -1,6 +1,7 @@
 // import Home from './pages/Home'
 import { Layout } from 'antd'
 import Home from './pages/Home'
+import Login from './pages/Login'
 const { Header } = Layout
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
@@ -10,8 +11,10 @@ function App() {
   return (
     <Router>
       <Header className='header'>Zelene Blockchain</Header>
-      <Home />
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>      
     </Router>
   )
 }
